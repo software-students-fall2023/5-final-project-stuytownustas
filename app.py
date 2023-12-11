a9e8937372d24c14dcd7d00c3b786d2fc0a54e7a
James Luo <james@10-17-32-20.dynapool.wireless.nyu.edu>
2023-12-11 13:06:34 -0500

    ,

--- app.py
+++ app.py
@@ -25,13 +25,20 @@ def index():
 @app.route('/tasks', methods=['GET', 'POST'])
 def manage_tasks():
     if request.method == 'GET':
-        task_list = list(tasks.find())
+        # Retrieve search query if provided
+        search_query = request.args.get('search', '').strip()
+
+        # Query tasks based on the search query
+        if search_query:
+            task_list = list(tasks.find({'description': {'$regex': search_query, '$options': 'i'}}))
+        else:
+            task_list = list(tasks.find())
 
         # Convert ObjectId to string for JSON serialization
         for task in task_list:
             task['_id'] = str(task['_id'])
 
-        return render_template('tasks.html', tasks=task_list)
+        return render_template('tasks.html', tasks=task_list, search_query=search_query)
 
     elif request.method == 'POST':
         data = request.form.get('description', '')
@@ -42,6 +49,22 @@ def manage_tasks():
             return redirect(url_for('manage_tasks'))
         else:
             return jsonify({'error': 'Task description cannot be empty'}), 400
+        
+@app.route('/tasks/delete/<string:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task_id = ObjectId(task_id)
+    tasks.delete_one({'_id': task_id})
+    return redirect(url_for('manage_tasks'))
+
+@app.route('/tasks/delete-page')
+def delete_tasks_page():
+    task_list = list(tasks.find())
+
+    # Convert ObjectId to string for JSON serialization
+    for task in task_list:
+        task['_id'] = str(task['_id'])
+
+    return render_template('delete_tasks.html', tasks=task_list)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5001)
